9abc0e0b09f089c777776849fd3f72f01a41f9b0
ashutosh24sharma <151420381+ashutosh24sharma@users.noreply.github.com>
2024-11-17 15:40:24 +0530

    redux
    redux

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ function App() {
     <div className="flex juatify-center px-12 py-7 items-center flex-col ">
       <div className="items-center flex flex-col juatify-center ">
       <h1 className="bg-gradient-to-r from-pink-600 via-slate-100 text-2xl font-serif font-bold mb-5 to-purple-400 bg-clip-text md:text-5xl tracking-tight text-transparent">INcreament/ Decrement Counter</h1>
-      <h2 className="bg-gradient-to-r from-green-600 via-slate-100 to-pink-400 bg-clip-text text-3xl tracking-tight text-transparent">Using React and Readux</h2>
+      <h2 className="bg-gradient-to-r from-green-600 via-slate-100 to-pink-400 bg-clip-text text-3xl tracking-tight text-transparent">Using React and Redux</h2>
       </div>
       <div className="h-8 w-8 flex bg-white  flex-row justify-center items-center m-16">
       <button className="bg-green-200 md:text-10 px-3 py-3 text-4xl" onClick={()=> dispatch(dicNumber())}>-</button>
